cced85d3b424476edcbc607a02d971064dc6148c
alexandre <alexandrehainy12@gmail.com>
2020-12-23 16:46:01 +0100

    finished ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -2,5 +2,10 @@ module.exports = (client) => {
 
     console.log("J'suis ok mek !");
 
-    client.user.setActivity("Patou le BG", {type: 'WATCHING'})
+    let activiter = ['Patou le BG','Le Necronomicon pour les Nuls','Made By : Le mec qui n\'a aucun aim','Est-ce que j\'ai vraiment besoin d\'une activité ?']
+
+    setInterval(() => {
+	const index = Math.floor(Math.random() * (activiter.length - 1) + 1);
+	client.user.setActivity(activiter[index]);
+    }, 15000);
 };
