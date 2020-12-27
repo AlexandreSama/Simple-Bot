b93a1ef2da4b46b75a3370067407bbc5154591b7
AlexandreSama <alexandrehainy12@gmail.com>
2020-12-27 15:24:16 +0100

    add element in array

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
 
     console.log("J'suis ok mek !");
 
-    let activiter = ['Patou le BG','Le Necronomicon pour les Nuls','Made By : Le mec qui n\'a aucun aim','Est-ce que j\'ai vraiment besoin d\'une activité ?', 'Alexandre ? Connais pas', 'Un jour, la Dame arrivera a stream sans lag !', 'Robox ? Trop haut lvl pour moi', 'La légende dit que El Pigeon serait un oiseau ? Ou est-ce un avion ?']
+    let activiter = ['Patou le BG','Le Necronomicon pour les Nuls','Made By : Le mec qui n\'a aucun aim','Est-ce que j\'ai vraiment besoin d\'une activité ?', 'Alexandre ? Connais pas', 'Un jour, la Dame arrivera a stream sans lag !', 'Robox ? Trop haut lvl pour moi', 'La légende dit que El Pigeon serait un oiseau ? Ou est-ce un avion ?', '"On me prend feu !!!" Alexandre 2020']
 
     setInterval(() => {
 	const index = Math.floor(Math.random() * (activiter.length - 1) + 1);
