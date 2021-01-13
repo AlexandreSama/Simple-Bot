780530a80971c7122daf5a7f94c2e2cde4c4347a
AlexandreSama <alexandrehainy12@gmail.com>
2021-01-13 19:10:06 +0100

    add package

--- Command/play.js
+++ Command/play.js
@@ -1,28 +1,71 @@
-const Discord = require('discord.js');
-const { Player } = require("discord-player");
+const { Util } = require('discord.js');
+const ytdl = require('ytdl-core');
 
-module.exports.run = (client, message) => {
+module.exports = {
+	name: 'play',
+	description: 'Play command.',
+	usage: '[command name]',
+	args: true,
+	cooldown: 5,
+	async execute(message, args) {
+		const { channel } = message.member.voice;
+		if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+		const permissions = channel.permissionsFor(message.client.user);
+		if (!permissions.has('CONNECT')) return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+		if (!permissions.has('SPEAK')) return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
 
-    settings = {
-        prefix: ":",
-        token: "Your Discord Token"
-    };
+		const serverQueue = message.client.queue.get(message.guild.id);
+		const songInfo = await ytdl.getInfo(args[0].replace(/<(.+)>/g, '$1'));
+		const song = {
+			id: songInfo.videoDetails.video_id,
+			title: Util.escapeMarkdown(songInfo.videoDetails.title),
+			url: songInfo.videoDetails.video_url
+		};
 
-    const player = new Player(client);
-    // To easily access the player
-    client.player = player;
-    // add the trackStart event so when a song will be played this message will be sent
-    client.player.on('trackStart', (message, track) => message.channel.send(`Now playing ${track.title}...`))
+		if (serverQueue) {
+			serverQueue.songs.push(song);
+			console.log(serverQueue.songs);
+			return message.channel.send(`✅ **${song.title}** has been added to the queue!`);
+		}
 
-    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+		const queueConstruct = {
+			textChannel: message.channel,
+			voiceChannel: channel,
+			connection: null,
+			songs: [],
+			volume: 2,
+			playing: true
+		};
+		message.client.queue.set(message.guild.id, queueConstruct);
+		queueConstruct.songs.push(song);
 
-    // !play Despacito
-    // will play "Despacito" in the member voice channel
+		const play = async song => {
+			const queue = message.client.queue.get(message.guild.id);
+			if (!song) {
+				queue.voiceChannel.leave();
+				message.client.queue.delete(message.guild.id);
+				return;
+			}
 
-    client.player.play(message, args[0]);
-}
+			const dispatcher = queue.connection.play(ytdl(song.url))
+				.on('finish', () => {
+					queue.songs.shift();
+					play(queue.songs[0]);
+				})
+				.on('error', error => console.error(error));
+			dispatcher.setVolumeLogarithmic(queue.volume / 5);
+			queue.textChannel.send(`🎶 Start playing: **${song.title}**`);
+		};
 
-module.exports.help = {
-    name: 'play'
-}
+		try {
+			const connection = await channel.join();
+			queueConstruct.connection = connection;
+			play(queueConstruct.songs[0]);
+		} catch (error) {
+			console.error(`I could not join the voice channel: ${error}`);
+			message.client.queue.delete(message.guild.id);
+			await channel.leave();
+			return message.channel.send(`I could not join the voice channel: ${error}`);
+		}
+	}
+};
