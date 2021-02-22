10a795c82e6ba9ee5c2cbf957b7efa46aef4240a
AlexandreSama <alexandrehainy12@gmail.com>
2021-02-22 21:10:04 +0100

    fix repeat cmd & add stop repeat cmd

--- Command/stoprepeat.js
+++ Command/stoprepeat.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
 
-    client.player.setRepeatMode(message.guild.id, true);
-        // Get the current song
+    client.player.setRepeatMode(message.guild.id, false);
+    // Get the current song
     let song = await client.player.nowPlaying(message.guild.id);
-    message.channel.send(`${song.name} will be repeated indefinitely!`);
+    message.channel.send(`${song.name}  will no longer be repeated indefinitely!`);
     
 }
 
 module.exports.help = {
-    name: 'repeat'
+    name: 'srepeat'
 }
